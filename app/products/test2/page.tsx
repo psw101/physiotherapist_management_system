6c9bc26f1a9e8899f059c4e14b7f93b6044afc67
Pulindu Wimalaweera <pulindusathruwan@gmail.com>
2025-05-11 12:34:54 +0530

    merging test on master and dev branch

--- app/products/test2/page.tsx
+++ app/products/test2/page.tsx
@@ -6,7 +6,7 @@ const ProductFormPage = () => {
   return (
     <div className="max-w-xl">
       <TextField.Root placeholder="Search the docs…" />
-        
+      <h1>Hello</h1>
      
     </div>
   );
